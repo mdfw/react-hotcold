f09fac36072d1e3de833736f12dcb6c21ad83d54
Mark Williams <github@shymedia.com>
2016-12-12 22:56:11 -0800

    Added thunk and the redux devtools for debugging.

--- js/store.js
+++ js/store.js
@@ -1,8 +1,14 @@
 var redux = require('redux');
 var createStore = redux.createStore;
+var compose = redux.compose;
+var applyMiddleware = redux.applyMiddleware;
+var thunk = require('redux-thunk').default;
 
 var reducers = require('./reducers/index');
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-var store = createStore(reducers.hotcoldReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+var store = createStore(reducers.hotcoldReducer, composeEnhancers(
+    applyMiddleware(thunk)
+  ));
 
 module.exports  = store;

4f46dd780227c77afeab1b66e451be883f527845
Mark Williams <github@shymedia.com>
2016-12-13 21:44:45 -0800

    Classed up the guess processer

--- js/reducers/index.js
+++ js/reducers/index.js
@@ -12,22 +12,21 @@ var hotcoldReducer = function(state = createNewState(), action) {
 		
 		case actions.NEW_GUESS:
 			var newstate = Object.assign({}, state);
-			var validated = validateGuess(action.guess);
+			var validated = reckon.validate(action.guess);
 			if (!validated.valid) {
 				newstate.basefeedback = validated.errormsg;
 				newstate.relativefeedback = null;
 			} else {
-				newstate.basefeedback = baseResponseFromGuess(state.target, validated.parsedGuess);
+				newstate.basefeedback = reckon.baseResponse(state.target, validated.guess);
 				var lastguess = state.guesses[state.guesses.length -1];
-				newstate.relativefeedback = relativeResponseFromGuess(validated.parsedGuess, lastguess, state.target);
+				newstate.relativefeedback = reckon.relativeResponse(validated.guess, lastguess, state.target);
 				newstate.currentguessinput = "";
-				newstate.guesses = newstate.guesses.concat(validated.parsedGuess);
+				newstate.guesses = newstate.guesses.concat(validated.guess);
 			}
 			return newstate;
 			break;
 			
 		case actions.FETCH_FEWEST_SUCCESS:
-		  console.log("got new fewest for state: " + action.fewest);
 			var newstate = Object.assign({}, state);
 			newstate.fewestguesses = action.fewest;
       return newstate;
@@ -38,7 +37,6 @@ var hotcoldReducer = function(state = createNewState(), action) {
 	}
     return state;
 };
-
 
 function createNewState(newtarget, fewest) {
 	var theTarget = newtarget;
@@ -60,71 +58,73 @@ function createNewState(newtarget, fewest) {
 	}
 }
 
-var validateGuess = function(guess) {
-	var validate = {
-		valid: true,
-		errormsg: "",
-		parsedguess: NaN
-	}
-	if (!guess) {
-		validate.valid = false;
-		validate.errormsg = constants.VALIDATION_ERROR_NOGUESS;
-		return validate;
-	}
-	var parsedGuess = parseInt(guess);
-	if (isNaN(parsedGuess)) {
-		validate.valid = false;
-		validate.errormsg = constants.VALIDATION_ERROR_NAN;
-		return validate;
-	} 
-	if (parsedGuess < 1 || parsedGuess > 100) {
-		validate.valid = false;
-		validate.errormsg = constants.VALIDATION_ERROR_OUTOFBOUNDS;
-		return validate;
-	}
-	validate.parsedGuess = parsedGuess;
-	return validate;
+var reckon = {
+  validate:function(guess) {
+    var validated = {
+		  valid: true,
+      errormsg: "",
+      guess: NaN
+	  }
+	  var parsedGuess = parseInt(guess);
+	  
+  	if (!parsedGuess) {
+  		validated.valid = false;
+  		validated.errormsg = constants.VALIDATION_ERROR_NOGUESS;
+  		return validated;
+  	}
+  	if (isNaN(parsedGuess)) {
+  		validated.valid = false;
+  		validated.errormsg = constants.VALIDATION_ERROR_NAN;
+  		return validated;
+  	} 
+  	if (parsedGuess < 1 || parsedGuess > 100) {
+      validated.valid = false;
+  		validated.errormsg = constants.VALIDATION_ERROR_OUTOFBOUNDS;
+  		return validated;
+  	}
+  	validated.guess = parsedGuess;
+  	return validated;
+  },
+  
+  /* Returns Very cold, cold, hot and very hot */
+  baseResponse: function(target, guess) {
+  	if (target == guess) {
+  		return constants.BASE_CORRECT;
+  	}
+  	var distance = Math.abs(target - guess);
+  	if (distance > 50) {
+  	 	return constants.BASE_ICECOLD;
+  	} else if (distance > 30) {
+  		return constants.BASE_COLD;
+  	} else if (distance > 20) {
+  		return constants.BASE_WARM;
+  	} else if (distance > 10) {
+  		return constants.BASE_HOT;
+  	} else {
+  		return constants.BASE_SUPERHOT;
+  	}
+  	return constants.BASE_HRM;
+  },
+
+  /* Returns "hotter than X" or "colder than x" */
+  relativeResponse: function (thisGuess, lastGuess, target) {
+  	if (!lastGuess || thisGuess == target) {
+  		return null;
+  	}
+  	if (lastGuess === thisGuess) {
+  		return constants.RELATIVE_SAMEAS;
+  	}
+  	var currentDistance = Math.abs(target - thisGuess);
+  	var previousDistance = Math.abs(target - lastGuess);
+  	if (currentDistance > previousDistance) {
+  		return constants.RELATIVE_COOLER;
+  	} else {
+  		return constants.RELATIVE_WARMER;
+  	}
+  	return null;
+  }
+
 }
-
-
-/* Returns Very cold, cold, hot and very hot */
-var baseResponseFromGuess = function(target, guess) {
-	if (target == guess) {
-		return constants.BASE_CORRECT;
-	}
-	var distance = Math.abs(target - guess);
-	if (distance > 50) {
-	 	return constants.BASE_ICECOLD;
-	} else if (distance > 30) {
-		return constants.BASE_COLD;
-	} else if (distance > 20) {
-		return constants.BASE_WARM;
-	} else if (distance > 10) {
-		return constants.BASE_HOT;
-	} else {
-		return constants.BASE_SUPERHOT;
-	}
-	return constants.BASE_HRM;
-}
-
-/* Returns "hotter than X" or "colder than x" */
-var relativeResponseFromGuess = function (thisGuess, lastGuess, target) {
-	if (!lastGuess || thisGuess == target) {
-		return null;
-	}
-	if (lastGuess === thisGuess) {
-		return constants.RELATIVE_SAMEAS;
-	}
-	var currentDistance = Math.abs(target - thisGuess);
-	var previousDistance = Math.abs(target - lastGuess);
-	if (currentDistance > previousDistance) {
-		return constants.RELATIVE_COOLER;
-	} else {
-		return constants.RELATIVE_WARMER;
-	}
-	return null;
-}
-
 
 exports.hotcoldReducer = hotcoldReducer;
 
